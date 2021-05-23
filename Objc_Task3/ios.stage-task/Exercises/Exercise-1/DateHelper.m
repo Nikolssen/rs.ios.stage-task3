c42c9da6cd04814bd74e7c6b7162e480b4f63b43
Nikolssen <ibudovichivan3@gmail.com>
2021-05-23 18:52:17 +0300

    Add: Task3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,24 +5,52 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    if (monthNumber == 0 || monthNumber > 12)
+        return nil;
+    
+    NSDateFormatter* formatter = [NSDateFormatter new];
+    NSString* name = [[formatter monthSymbols] objectAtIndex:(monthNumber - 1)];
+    return name;
+        
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSString* format = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSDateFormatter* formatter = [NSDateFormatter new];
+    formatter.dateFormat = format;
+    NSCalendar* calendar = [NSCalendar currentCalendar];
+    NSDate* theDate = [formatter dateFromString:date];
+    return [calendar component:NSCalendarUnitDay fromDate: theDate];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSCalendar* calendar = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSLocale* locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru"];
+    NSDateFormatter* formatter = [NSDateFormatter new];
+    formatter.locale = locale;
+    formatter.calendar = calendar;
+    formatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:10800];
+    formatter.dateFormat = @"E";
+    return [formatter stringFromDate:date];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
+    NSCalendar* calendar = [NSCalendar calendarWithIdentifier:NSCalendarIdentifierGregorian];
+    calendar.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru"];
+    calendar.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:10800];
+    
+    NSDateComponents* currentDateComponents = [calendar components:NSCalendarUnitYear|NSCalendarUnitWeekOfYear fromDate:[NSDate now]];
+    NSDateComponents* dateComponents = [calendar components:NSCalendarUnitYear|NSCalendarUnitWeekOfYear fromDate: date];
+    if (currentDateComponents.year == dateComponents.year) {
+        return currentDateComponents.weekOfYear == dateComponents.weekOfYear;
+    }
     return NO;
 }
 
--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,35 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    if (string1.length == 0)
+        return string2;
+    if (string2.length == 0)
+        return string1;
+    
+    int str1CurrentChar = 0;
+    int str2CurrentChar = 0;
+    NSMutableString *result = [NSMutableString new];
+    
+    while (str1CurrentChar < string1.length && str2CurrentChar < string2.length) {
+        NSString* str1Char = [string1 substringWithRange:NSMakeRange(str1CurrentChar, 1)];
+        NSString* str2Char = [string2 substringWithRange:NSMakeRange(str2CurrentChar, 1)];
+        
+        if ([str1Char compare: str2Char] != NSOrderedDescending) {
+            [result appendString:str1Char];
+            str1CurrentChar++;
+        }
+        else {
+            [result appendString:str2Char];
+            str2CurrentChar++;
+        }
+    }
+    if (str1CurrentChar == string1.length){
+        [result appendString:[string2 substringFromIndex:str2CurrentChar]];
+    }
+    else {
+        [result appendString:[string1 substringFromIndex:str1CurrentChar]];
+    }
+    return result;
 }
 
 @end
